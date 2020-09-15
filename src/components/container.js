427d49f218078008a393957902db6f0997ff695f
Anmol Chaudhary <chaudharyanmol321@gmail.com>
2020-09-15 17:40:26 +0530

    x

---
+++ src/components/container.js
@@ -0,0 +1,8 @@
+import React from "react"
+import "../styles/container.css"
+
+const Container = ({ children }) => (
+  <div className="container"> {children} </div>
+)
+
+export default Container
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,18 @@
 import React from "react"
-import { Link } from "gatsby"
-
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import { PrimaryButton } from "reaction-ui"
+import Container from "../components/container"
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
+  <Container>
+    <h1> Hi people </h1>
+    <p>
+      Welcome to your new Gatsby site. sdsdsjghsjgfhsgf sfhsjfgsjfhsgf
+      fksjhfjshfkjshfkjshf fksjfhsfk jshfjkshfjks fskfhskjfhskhfjkshf
+    </p>
+    <p> Now go build something great. </p>
+    <PrimaryButton shadow borderRadius={4}>Hello World    </PrimaryButton>
+    <PrimaryButton shadow borderRadius={4}>      hello World    </PrimaryButton>
+  </Container>
 )
 
 export default IndexPage
