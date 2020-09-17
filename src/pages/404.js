19a690699692b77719fac669a0718acaa7630f1f
Anmol Chaudhary <chaudharyanmol321@gmail.com>
2020-09-17 19:49:15 +0530

    Update 404.js

---
+++ src/pages/404.js
@@ -0,0 +1,38 @@
+import { Link } from "gatsby";
+import React from "react";
+import { PrimaryButton, Font, Stack } from "reaction-ui";
+import Container from "../components/container";
+import Logo from "../components/Logo";
+import { Helmet } from "react-helmet";
+
+const ErrorPage = () => (
+  <Container>
+    <Helmet>
+      <meta charSet="utf-8" />
+      <title>Reaction UI</title>
+    </Helmet>
+    <Stack direction="column" gap={10}>
+      <Font fontSize={32} fontFamily="Gilroy-Extrabold">
+        <span role="img" aria-label="emoji">
+          🤜
+        </span>
+        Hi people
+        <span role="img" aria-label="emoji">
+          🤛
+        </span>
+      </Font>
+      <Font fontWeight={800} fontSize={20} textAlign="center">
+        Reaction UI comes with a set of components built with styled-components <br />
+        <center>that are accessible, responsive and customisable.</center>
+      </Font>
+      <Logo width={600} />
+      <Link to="/getting-started" className="link">
+        <PrimaryButton shadow borderRadius={4} bgColor="#3c122c">
+          Getting Started
+        </PrimaryButton>
+      </Link>
+    </Stack>
+  </Container>
+);
+
+export default ErrorPage;
